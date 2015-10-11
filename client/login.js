145ea54733b4b5a44ecb4491e97e398788a35122
Yousuf Hussain <yousuf.1991@hotmail.com>
2015-10-11 01:11:16 -0700

    Merge branch 'master' of https://github.com/Opportunity-Hack-2015-Arizona/Team3
    # Conflicts:
    #	.idea/simple-todos.iml
    #	.idea/workspace.xml

--- client/login.js
+++ client/login.js
@@ -1,4 +1,3 @@
-
 if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
@@ -14,12 +13,37 @@ if (Meteor.isClient) {
             // Get value from form element
             var personName = event.target.personName.value;
             var personAge = event.target.personAge.value;
+            var cancerType = event.target.cancerType.value;
+            var gender = event.target.gender.value;
 
             // Insert a task into the collection
+
             if (Meteor.userId()) {
-                 Meteor.users.update({_id: Meteor.userId()},
-                     { $set: { 'profile.name': personName, 'profile.age': personAge }});
+                var flag = false;
+                Meteor.users.update({_id: Meteor.userId()},
+                    {
+                        $set: {
+                            'profile.name': personName,
+                            'profile.age': personAge,
+                            'profile.cancerType': cancerType,
+                            'profile.gender': gender
+                        }
+                    });
+                $('#form-div').modal('hide');
             }
+            $('#form-div').modal('hide');
+        }
+    });
+    Template.body.events({
+        'click #settings': function (e) {
+            e.preventDefault();
+            $('#form-div').modal('show');
+        }
+    });
+    Template.body.events({
+        'click .glyphicon-remove': function (e) {
+            e.preventDefault();
+            $('#form-div').modal('hide');
         }
     });
     Accounts.ui.config({
