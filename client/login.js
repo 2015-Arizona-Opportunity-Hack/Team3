87ea53cc6a744bc0c9d98896a25ea4aae1827505
Yousuf Hussain <yousuf.1991@hotmail.com>
2015-10-10 21:49:31 -0700

    Merge branch 'master' of https://github.com/Opportunity-Hack-2015-Arizona/Team3
    # Conflicts:
    #	.idea/simple-todos.iml
    #	.idea/workspace.xml

--- client/login.js
+++ client/login.js
@@ -1,5 +1,3 @@
-Tasks = new Mongo.Collection("tasks");
-
 if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
@@ -13,52 +11,17 @@ if (Meteor.isClient) {
             event.preventDefault();
 
             // Get value from form element
-            var text = event.target.text.value;
+            var personName = event.target.personName.value;
+            var personAge = event.target.personAge.value;
 
             // Insert a task into the collection
-            Meteor.call("addTask", text);
-
-            // Clear form
-            event.target.text.value = "";
-        }
-    });
-    Template.task.events({
-        "click .toggle-checked": function () {
-            // Set the checked property to the opposite of its current value
-            Meteor.call("setChecked", this._id, !this.checked);
-        },
-        "click .delete": function () {
-            Meteor.call("deleteTask", this._id);
+            if (Meteor.userId()) {
+                 Meteor.users.update({_id: Meteor.userId()},
+                     { $set: { 'profile.name': personName }});
+            }
         }
     });
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
     });
 }
-Meteor.methods({
-    addTask: function (text) {
-        // Make sure the user is logged in before inserting a task
-        if (!Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-        var name = Meteor.user().username;
-        if (name === undefined) {
-            var queryDocument = Meteor.users.findOne(
-                {_id: Meteor.userId()},
-                {'profile.name': 1});
-            name = queryDocument.profile.name;
-        }
-        Tasks.insert({
-            text: text,
-            createdAt: new Date(),
-            owner: Meteor.userId(),
-            username: name
-        });
-    },
-    deleteTask: function (taskId) {
-        Tasks.remove(taskId);
-    },
-    setChecked: function (taskId, setChecked) {
-        Tasks.update(taskId, {$set: {checked: setChecked}});
-    }
-});
