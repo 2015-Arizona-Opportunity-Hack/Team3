e7b97dc6e208cf0a087f08318c3663e5d34199a1
Nitesh Thali <niteshthali08@gmail.com>
2015-10-11 04:34:19 -0700

    testing router implementation.

---
+++ router.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nitesh on 10/11/15.
+ */
+
+
+Router.map(function(){
+    this.route('login', {path: '/'});
+    this.route('getinfo', {path: '/getinfo'});
+    this.route('donate', {path:'/donate'});
+    this.route('about', {path:'/about'});
+
+});
